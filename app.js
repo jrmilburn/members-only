c093549a27e9ad055961541b98e791fc781e1d64
Joe Milburn <jrmilburn@outlook.com>
2024-08-16 21:23:58 +1000

    Set PORT in .env

--- app.js
+++ app.js
@@ -20,5 +20,6 @@ app.use(passport.session());
 
 app.use("/", indexRouter);
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log("app listening on port 3000"));
+app.listen(PORT, () => console.log("app listening on port 3000"));
